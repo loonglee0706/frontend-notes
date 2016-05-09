74cccea5d44627aea12f8ecd1903a3e0e05e5b44
lifei <loonglee0706@126.com>
2016-05-09 15:53:03 +0800

    url querystring param value function

---
+++ url_querystring.js
@@ -0,0 +1,18 @@
+/*
+ * get url querystring params' value
+ */
+function $_GET(param) {
+    var vars = {};
+    var hash = src.indexOf('#') >= 0 ? src.slice(src.indexOf('#')) : '';
+    src && src.replace( hash , '' ).replace( 
+        /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+        function( m, key, value ) { // callback
+            vars[key] = value !== undefined ? value : '';
+        }
+    );
+
+    if ( param ) {
+        return vars[param] ? vars[param] : null;    
+    }
+    return vars;
+}
